c1aa57f0abc24b5c57a653d32f79533a7c2cbc77
Desu1243 <lubonskimateusz@gmail.com>
2023-02-14 18:20:38 +0100

    comments

--- lib/models/FlashCard.dart
+++ lib/models/FlashCard.dart
@@ -1,5 +1,9 @@
 class FlashCard{
-  late String term;
-  late String definition;
-  FlashCard({required this.term, required this.definition});
+  late String term; //term user wants to learn
+  late String definition; //definition (or translation) of that term
+
+  FlashCard({
+    required this.term,
+    required this.definition
+  });
 }
--- lib/models/FlashCardCollection.dart
+++ lib/models/FlashCardCollection.dart
@@ -1,7 +1,11 @@
 import 'FlashCard.dart';
 
 class FlashCardCollection{
-  late String title;
-  late List<FlashCard> collection;
-  FlashCardCollection({required this.title, required this.collection});
+  late String title; //title of the collection, shown on the home screen
+  late List<FlashCard> collection; //flash cards that are part of a collection
+
+  FlashCardCollection({
+    required this.title,
+    required this.collection
+  });
 }
