077e0199f704bc47b19a2fb0cfa4cca9c9f6b40f
Desu1243 <lubonskimateusz@gmail.com>
2023-03-07 12:47:42 +0100

    new theme

--- lib/main.dart
+++ lib/main.dart
@@ -40,17 +40,44 @@ List<ThemeData> themes = [
     ),
     brightness: Brightness.light,
   ),
+
+  /// Blue sky
+  ThemeData(
+    colorScheme: const ColorScheme(
+      brightness: Brightness.light,
+      primary: Color.fromRGBO(74, 26, 86, 1.0),
+      secondary: Color.fromRGBO(238, 207, 249, 1.0),
+      background: Color.fromRGBO(51, 8, 58, 1.0),
+      surface: Color.fromRGBO(199, 99, 224, 1.0),
+      onPrimary: Color.fromRGBO(88, 16, 114, 1.0),
+
+      error: Color.fromRGBO(1, 1, 1, 0.0),
+      onSecondary: Color.fromRGBO(1, 1, 1, 0.0),
+      onBackground: Color.fromRGBO(1, 1, 1, 0.0),
+      onSurface: Color.fromRGBO(101, 239, 99, 1.0), //E06363 //red
+      onError: Color.fromRGBO(249, 99, 99, 1.0), //65E063 //green
+    ),
+    brightness: Brightness.light,
+  ),
 ];
 
 Future<void> getTheme() async{
   var themeIndex = 0;
-  var db = await openDatabase('fisher.db');
-  await db.execute("CREATE TABLE IF NOT EXISTS theme (id INT NOT NULL, theme_number INT NOT NULL, PRIMARY KEY (id))");
-  List<Map> dbThemes = await db.rawQuery('SELECT * FROM theme');
-  if(dbThemes.isEmpty){
-    await db.transaction((txn) async {int id1 = await txn.rawInsert('INSERT INTO theme(id, theme_number) VALUES(1, 0)');});
-  }else{
-    themeIndex = dbThemes[0]['theme_number'];
+  try {
+    var db = await openDatabase('fisher.db');
+    await db.execute(
+        "CREATE TABLE IF NOT EXISTS theme (id INT NOT NULL, theme_number INT NOT NULL, PRIMARY KEY (id))");
+    List<Map> dbThemes = await db.rawQuery('SELECT * FROM theme');
+    if (dbThemes.isEmpty) {
+      await db.transaction((txn) async {
+        int id1 = await txn.rawInsert(
+            'INSERT INTO theme(id, theme_number) VALUES(1, 0)');
+      });
+    } else {
+      themeIndex = dbThemes[0]['theme_number'];
+    }
+  }catch(e){
+    print(e);
   }
 
   runApp(MaterialApp(
