94767ab9e211659bdeec51e31d24de6580a0f737
Desu1243 <lubonskimateusz@gmail.com>
2023-03-12 11:58:22 +0100

    dynamic flash card creation form

---
+++ lib/widgets/FlashCardFormItemWidget.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:fisher/models/FlashCard.dart';
+
+class FlashCardFormItemWidget extends StatefulWidget {
+  FlashCardFormItemWidget({super.key, required this.flashCard});
+
+  FlashCard flashCard;
+  final TextEditingController _termController = TextEditingController();
+  final TextEditingController _definitionController = TextEditingController();
+
+  @override
+  State<StatefulWidget> createState() => _FlashCardFormItemWidgetState();
+}
+
+class _FlashCardFormItemWidgetState extends State<FlashCardFormItemWidget> {
+  final formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    ColorScheme theme = Theme.of(context).colorScheme;
+
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 4.0),
+      child: Form(
+        key: formKey,
+        child: Container(
+          padding: EdgeInsets.all(8.0),
+          color: theme.primary,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              TextFormField(
+                controller: widget._termController,
+                onChanged: (value) => widget.flashCard.term = value,
+                onSaved: (value) => widget.flashCard.term = value!,
+                style: TextStyle(color: theme.secondary, fontSize: 20),
+                cursorColor: theme.secondary,
+                decoration: InputDecoration(
+                  focusedBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: theme.surface),
+                  ),
+                  enabledBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: theme.secondary),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 4.0),
+                child: Text("TERM", style: TextStyle(color: theme.secondary)),
+              ),
+
+              TextFormField(
+                controller: widget._definitionController,
+                onChanged: (value) => widget.flashCard.definition = value,
+                onSaved: (value) => widget.flashCard.definition = value!,
+                style: TextStyle(color: theme.secondary, fontSize: 20),
+                cursorColor: theme.secondary,
+                decoration: InputDecoration(
+                  focusedBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: theme.surface),
+                  ),
+                  enabledBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: theme.secondary),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 4.0),
+                child: Text("DEFINITION", style: TextStyle(color: theme.secondary)),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
